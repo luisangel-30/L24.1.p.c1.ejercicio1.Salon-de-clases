0bf68c5238cad008e06c1ce170bc460ec54ec307
luisangel-30 <1001.30845134.ucla@gmail.com>
2024-07-05 09:07:03 -0400

    Cambio a las 0906

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
+/*
+Sea un salón de clases en el DCyT, donde se procesan los datos de cada estudiante (nombre,
+edad y sexo). El dato sexo es una letra F o M (femenino - masculino). Se desean procesar estos
+registros de estudiantes para determinar: a. la edad promedio, b. el nombre del estudiante con
+mayor edad, c. el porcentaje de chicas que son mayor de edad.
+El profesor informa que si asisten Luis (16), Ana (19), José (20) y Carmen (18), la salida
+requerida presenta el siguiente formato:
+Edad promedio: 18.25
+Estudiante con mayor edad: José
+Porcentaje de chicas mayor de edad: 100%
+
+*/
+
 import CL_estudiante from "./CL_estudiante.js";
 import CL_salon from "./CL_salon.js";
 
